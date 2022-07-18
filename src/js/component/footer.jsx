bf665e88a93c8eb49fee09c868bb87a827dc1e32
Alex1kenobi <alebarto2002@gmail.com>
2022-07-18 19:00:48 +0000

    jumbo + tarjeta + footer

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+
+return (
+  <div className="footertext">
+    <h5 classname="footerh5text"> Copyright 2022 © </h5>
+  </div>
+);
+}
+
+
+
+
+export default Footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,12 +4,15 @@ import Jumbotron from "./jumbotron.jsx";
 
 import Cards from "./card.jsx";
 
+import Footer from "./footer.jsx";
+
 //create your first component
 const Home = () => {
 	return (
 		<>
 		<Jumbotron/>
 		<Cards/>
+		<Footer/>
 		</>
 	);
 };
