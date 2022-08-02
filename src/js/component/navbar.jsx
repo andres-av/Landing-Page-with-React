d648d1f3e852622b6e7edd56df0f8ae0a87171b4
andres-av <jandresav1793@gmail.com>
2022-08-02 09:11:52 +0000

    changed class to className on Navbar

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -2,29 +2,29 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav class="navbar bg-dark">
-      <div class="container-fluid">
-        <h5 class="text-white">
+    <nav className="navbar bg-dark">
+      <div className="container-fluid">
+        <h5 className="text-white">
           Navbar
         </h5>
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0 navBarFlex-container">
-          <li class="nav-item">
-            <a class="nav-link active text-white" aria-current="page" href="#">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0 navBarFlex-container">
+          <li className="nav-item">
+            <a className="nav-link active text-white" aria-current="page" href="#">
               Home
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active text-white" aria-current="page" href="#">
+          <li className="nav-item">
+            <a className="nav-link active text-white" aria-current="page" href="#">
               About
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active text-white" aria-current="page" href="#">
+          <li className="nav-item">
+            <a className="nav-link active text-white" aria-current="page" href="#">
               Services
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active text-white" aria-current="page" href="#">
+          <li className="nav-item">
+            <a className="nav-link active text-white" aria-current="page" href="#">
               Contact
             </a>
           </li>
